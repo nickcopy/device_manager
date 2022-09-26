d6c6f5850dd07f95f3522c9eadf27dd1479f2adb
LJW <golove010@gmail.com>
2022-09-26 13:19:57 +0900

    README add

---
+++ components/Counter.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+interface counterProps {
+  aboutName: String;
+  subName?: String;
+  startDate: Number;
+  amu?: {
+    aaa: Number;
+  };
+}
+export default function Counter(props: counterProps) {
+  let [count, setCount] = useState(0);
+  return (
+    <div>
+      <h2>카운터{count}</h2>
+      <div>{props.aboutName}</div>
+      <div>{props.startDate.toString()}</div>
+      <button onClick={() => setCount(count++)}>+1</button>
+      <button onClick={() => setCount(count--)}>-1</button>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from "next";
-
+import Counter from "../components/Counter";
 const Home: NextPage = () => {
-  return <div>hello</div>;
+  return (
+    <div>
+      hello
+      <Counter aboutName={"이진우"} startDate={20220926} />
+    </div>
+  );
 };
 
 export default Home;
